0b0edd6e756fa18f6fc56c3989071ccc2185f7c8
German Voytekhovich <German.Voytekhovich@firstlinesoftware.com>
2020-02-03 16:23:41 +0300

    fix indent, added gitignore

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+  
+FROM golang:1.13.7-buster
+
+WORKDIR /go/src/app
+COPY . .
+
+RUN go get -d -v
+RUN go build -v
+
+CMD ["./app"]
--- main.go
+++ main.go
@@ -1,18 +1,46 @@
 package main
 
 import (
-	// "fmt"
-	"log"
-	"github.com/gin-gonic/gin"
-	// "os"
+  "log"
+  "github.com/gin-gonic/gin"
+)
+
+const (
+  StatusOK = 200
+  StatusCreated = 201
+  
+  StatusBadRequest = 400
+  StatusForbidden = 403
+  StatusNotFound = 404
+  
+  StatusInternalServerError = 500
 )
 
 func main() {
-	log.Println("Starting app....")
-
-	r := gin.Default()
-
-	port := "3000"
-
-	r.Run(":" + port)
+  log.Println("Starting app")
+  
+  r := gin.Default()
+  
+  // r.POST("/api/v1/upload", uploadFile)
+  // r.POST("/api/v1/image/resize", resizeImage)
+  // r.GET("/api/v1/image", getImage)
+  
+  port := "3000"
+  
+  r.Run(":" + port)
+  
+  log.Println("App is ready")
 }
+
+// func uploadFile(c *gin.Context) {
+
+// }
+
+// func resizeImage(c *gin.Context) {
+
+// }
+
+// func getImage(c *gin.Context) {
+
+// }
+      
