113fb714547bbed6b62b4be05baf16cb934e4579
German Voytekhovich <ueuknjg@gmail.com>
2020-02-04 10:04:17 +0300

    added upload file

--- main.go
+++ main.go
@@ -1,46 +1,79 @@
 package main
 
 import (
-  "log"
-  "github.com/gin-gonic/gin"
-)
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+	"time"
 
-const (
-  StatusOK = 200
-  StatusCreated = 201
-  
-  StatusBadRequest = 400
-  StatusForbidden = 403
-  StatusNotFound = 404
-  
-  StatusInternalServerError = 500
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 func main() {
-  log.Println("Starting app")
-  
-  r := gin.Default()
-  
-  // r.POST("/api/v1/upload", uploadFile)
-  // r.POST("/api/v1/image/resize", resizeImage)
-  // r.GET("/api/v1/image", getImage)
-  
-  port := "3000"
-  
-  r.Run(":" + port)
-  
-  log.Println("App is ready")
+	log.Println("Starting app")
+
+	r := gin.Default()
+
+	r.POST("/api/v1/file", uploadFile)
+
+	port := "3000"
+
+	r.Run(":" + port)
+
+	log.Println("App is ready")
 }
 
-// func uploadFile(c *gin.Context) {
+func uploadFile(c *gin.Context) {
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"status": "File not informed"})
+		return
+	}
 
-// }
+	filename := header.Filename
+	groupName := getGroupFileName()
 
-// func resizeImage(c *gin.Context) {
+	dirname := fmt.Sprint("files/", groupName)
+	fmt.Println(getFileUUIDName(filename))
+	newfilename := fmt.Sprint(dirname, "/"+getFileUUIDName(filename))
 
-// }
+	if _, err := os.Stat(dirname); os.IsNotExist(err) {
+		os.Mkdir(dirname, 0700)
+	}
 
-// func getImage(c *gin.Context) {
+	out, err := os.Create(newfilename)
+	if err != nil {
+		log.Println("[ERROR] Creating file error", err)
+		c.JSON(http.StatusNotFound, gin.H{"status": "[ERROR] Creating file error " + filename})
+		return
+	}
 
-// }
-      
+	defer out.Close()
+
+	_, err = io.Copy(out, file)
+
+	if err != nil {
+		log.Println("[ERROR]: Saving file error", err)
+		c.JSON(http.StatusNotFound, gin.H{"status": "[ERROR]: Saving file error" + filename})
+		return
+	}
+}
+
+func getGroupFileName() string {
+	currentTime := time.Now()
+
+	return currentTime.Format("20060102")
+}
+
+func getFileUUIDName(filename string) string {
+	asd := strings.Split(filename, ".")
+	ext := asd[len(asd)-1]
+
+	name := uuid.New().String() + "." + ext
+
+	return name
+}
