7fba7794f3d0c38688471f21d8b16c2346de79e3
German Voytekhovich <German.Voytekhovich@firstlinesoftware.com>
2020-02-06 15:03:39 +0300

    added method for upload to s3

--- main.go
+++ main.go
@@ -8,15 +8,28 @@ import (
 	"os"
 
 	"./helpers"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
+}
 
 func main() {
 	log.Println("Starting app")
 
 	r := gin.Default()
-
-	r.POST("/api/v1/file", uploadFile)
+	r.POST("/api/v0/file", uploadFile)
+	r.POST("/api/v1/file", uploadFileS3)
 
 	port := "3000"
 
@@ -66,7 +79,68 @@ func uploadFile(c *gin.Context) {
 		return
 	}
 
-	pathname := "http://localhost:3000" + "/" + groupName + "/" + filename
+	pathname := "http://localhost:3000" + "/" + groupName + "/" + filename // todo
+
+	c.JSON(http.StatusCreated, gin.H{
+		"filename":  filename,
+		"extension": extension,
+		"size":      size,
+		"path":      pathname,
+	})
+}
+
+func uploadFileS3(c *gin.Context) {
+	accessKey := os.Getenv("AWS_SECRET_KEY")
+	secretKey := os.Getenv("AWS_ACCESS_KEY")
+
+	sess, err := session.NewSession(&aws.Config{
+		Region:   aws.String("fra1"),
+		Endpoint: aws.String("https://fra1.digitaloceanspaces.com"),
+		Credentials: credentials.NewStaticCredentials(
+			secretKey,
+			accessKey,
+			"",
+		),
+	})
+
+	if err != nil {
+		fmt.Println("Could not upload file")
+	}
+
+	file, header, err := c.Request.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"status": "File not informed"})
+		return
+	}
+
+	size := header.Size
+
+	if size > (1024 * 1024 * 5) {
+		c.JSON(http.StatusRequestEntityTooLarge, gin.H{"status": "[ERROR] File is too big"})
+		return
+	}
+
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"status": "[ERROR]: Saving file error" + "./Dockerfile"})
+		return
+	}
+
+	extension, filename := helpers.GetFileMeta(header.Filename)
+
+	groupName := helpers.GetGroupFileName()
+
+	_, s3err := s3.New(sess).PutObject(&s3.PutObjectInput{
+		Bucket: aws.String("aniki"),
+		Key:    aws.String(groupName + "/" + filename),
+		ACL:    aws.String("public-read"),
+		Body:   file,
+	})
+
+	if s3err != nil {
+		fmt.Println("asd")
+	}
+
+	pathname := "https://aniki.fra1.digitaloceanspaces.com" + "/" + groupName + "/" + filename // todo
 
 	c.JSON(http.StatusCreated, gin.H{
 		"filename":  filename,
